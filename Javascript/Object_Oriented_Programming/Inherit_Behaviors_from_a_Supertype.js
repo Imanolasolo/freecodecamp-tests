beefeae48c5c654645199dce2df9bee912d8dc69
Imanolasolo <jjusturi@gmail.com>
2022-03-31 12:27:45 -0500

    OOP exercises

---
+++ Javascript/Object_Oriented_Programming/Inherit_Behaviors_from_a_Supertype.js
@@ -0,0 +1,17 @@
+//Create the new duck and beagle objects using the Object.create() method
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+
+duck.eat(); // Should print "nom nom nom"
+beagle.eat(); // Should print "nom nom nom"
